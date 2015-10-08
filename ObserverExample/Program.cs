2ffa84c98e35ed6c653ff12545c354182679c68c
Abdul Yahya <abdullah.yahya@oit.edu>
2015-10-07 19:14:51 -0700

    Intitial Commit

---
+++ ObserverExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A ();
+            B b = new B ();
+            C c = new C ();
+
+            a.FruitSalad ( b );
+            a.FruitSalad ( c );
+        }
+    }
+
+    class A
+    {
+        public void FruitSalad ( I i )
+        {
+            i.fruit ( this );
+        }
+        public void apple ()
+        {
+            Console.WriteLine ( "Apple!" );
+        }
+    }
+    
+    interface I
+    {
+        void fruit ( A a );
+    }
+
+    class B : I
+    {
+        public void fruit ( A a )
+        {
+            a.apple ();
+            Console.WriteLine ( "Banana!" );
+        }
+    }
+
+    class C : I
+    {
+        public void fruit ( A a )
+        {
+            a.apple();
+            Console.WriteLine("Cantelope!");
+        }
+    }
+}
